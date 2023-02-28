6cb0b83a9474d6ad884d26ef72201225bd6dfeca
HIGHGROUNDFOREVER <111176938+HIGHGROUNDFOREVER@users.noreply.github.com>
2023-02-28 17:57:31 +0100

    1
    1

--- XML/UC/Sweden/3UC_SWEDEN.sql
+++ XML/UC/Sweden/3UC_SWEDEN.sql
@@ -2,7 +2,7 @@
 ------------------------------	
 INSERT INTO Buildings 	
 	(Type, BuildingClass, Cost, TradeRouteRecipientBonus, TradeRouteTargetBonus,  MaxStartEra, GoldMaintenance, PrereqTech, ArtDefineTag, XBuiltTriggersIdeologyChoice, SpecialistType, SpecialistCount, MinAreaSize, ConquestProb, HurryCostModifier, Help, Description, Civilopedia, Strategy, IconAtlas, PortraitIndex)
-SELECT	'BUILDING_3UC_STAVE_CHURCH', BuildingClass,  Cost, 1, 1, MaxStartEra, 0, PrereqTech, ArtDefineTag, XBuiltTriggersIdeologyChoice, SpecialistType, SpecialistCount, MinAreaSize, ConquestProb, HurryCostModifier, 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH_HELP', 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH', 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH_TEXT', 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH_STRATEGY', 'EXPANSION_SCEN_BUILDING_ATLAS', 0
+SELECT	'BUILDING_3UC_STAVE_CHURCH', BuildingClass,  Cost, 5, 5, MaxStartEra, 0, PrereqTech, ArtDefineTag, XBuiltTriggersIdeologyChoice, SpecialistType, SpecialistCount, MinAreaSize, ConquestProb, HurryCostModifier, 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH_HELP', 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH', 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH_TEXT', 'TXT_KEY_BUILDING_3UC_STAVE_CHURCH_STRATEGY', 'EXPANSION_SCEN_BUILDING_ATLAS', 0
 FROM Buildings WHERE Type = 'BUILDING_TEMPLE';	
 ------------------------------	
 -- Building_Flavors
